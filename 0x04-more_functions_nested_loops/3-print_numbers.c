580cb358cd6e7da9028637e6ca691e9dfa7c7f30
OKAFOR PRECIOUS <preciouschinazaekpere6@gmail.com>
2022-10-20 10:59:14 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks-for printed numbers
+ * @c: c is an ascii character
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(viod) 
+{
+        char number = '0';
+
+	while (number <= '9')
+	{
+	_putchar(letter);
+	letter++;
+	}
+	_putchar('\n');
+}	
