2e39a7f1a0a2bea824b359ad441c35ed76118e5e
Hush <preciouschinazaekpere6@gmail.com>
2022-10-20 19:38:16 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks-for printed numbers
+ *@c: c is an ascii character
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void) 
+{
+        char number = '0';
+       
+        while (number <= '9')
+        {
+          if( (number!=2) && (number!=4) )
+          {
+           _putchar(number);
+            number++;
+          }
+        }
+        _putchar ('\n');
+}
