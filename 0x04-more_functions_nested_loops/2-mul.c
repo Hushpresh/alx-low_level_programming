cd4bb4fc7c5bab8ff87a0fa34c69207718f1e0c5
OKAFOR PRECIOUS <preciouschinazaekpere6@gmail.com>
2022-10-20 10:44:19 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks-for multipy
+ * @c: c is an ascii characteer
+ *
+ * Return : always 0
+ */
+
+int mul(int a, int b)
+{
+	printf ("Enter both numbers\n");
+	scanf("%f %f" ,&num1,&num2);
+	return result;
+}	
