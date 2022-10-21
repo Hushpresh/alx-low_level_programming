88bebc7fdbf8d307039ab8ab74535f1668ec3b6a
Hush <preciouschinazaekpere6@gmail.com>
2022-10-21 01:11:52 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,18 @@
 
 
 /**
- *print_numbers - prints numbers 0-9
+ * print_numbers - prints numbers 0-9
  *
  * Return: void
  */
 
-void print_numbers(void) 
+void print_numbers(void)
 {
-  	int c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-}	
+}
