32eba8f63457a3fddb3cf9d2125a2b79c2133e2b
OKAFOR PRECIOUS <preciouschinazaekpere6@gmail.com>
2022-11-01 08:18:12 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * unsigned int _strspn - a function that gets the length of a prefix substring
+ * @s: initial segment
+ * @accept: bytes
+ * Return: the number of bytes in the initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k, tag;
+
+	k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		tag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				tag = 1;
+			}
+		}
+		if (tag == 0)
+		{
+			return (k);
+		}
+	}
+	return (0);
+}
