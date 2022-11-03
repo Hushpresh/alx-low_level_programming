5fd285f573fae2ca62bb4645ecff420bb69a4569
OKAFOR PRECIOUS <preciouschinazaekpere6@gmail.com>
2022-11-02 21:47:26 -0700

    print rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion -  prints a string in reverse.
- * @s:string to be printed in reverse
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
  *
  */
 void _print_rev_recursion(char *s)
@@ -11,5 +12,4 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 	}
-
 }
