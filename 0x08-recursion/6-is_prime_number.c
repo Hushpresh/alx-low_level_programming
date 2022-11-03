0e2189f04561b05228a20dd2986c16378a40d39c
OKAFOR PRECIOUS <preciouschinazaekpere6@gmail.com>
2022-11-02 21:27:53 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,40 @@
 #include "main.h"
+
+int tmp_prime(int n, int i);
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
- * Return: 1 if n is a prime number, 0 if not
+ * divisors - number is prime?
+ * @n: integer params
+ * @m: integer params
+ * Return: boolean
+ */
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+	return (0);
+	}
+	else if (m / 2 > n)
+	{
+	return (divisors(n + 2, m));
+	}
+	else
+	{
+	return (1);
+	}
+}
+/**
+ * is_prime_number - prime
+ * @n: integer params
+ * Return: recursion
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	if (n >= 2 && n <= 3)
-		return (1);
-	return (real_prime(n, n - 1));
-}
-
-/**
- * real_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-int real_prime(int n, int i)
-{
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (real_prime(n, i - 1));
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+	return (0);
+	}
+	else
+	{
+	return (divisors(3, n));
+	}
 }
