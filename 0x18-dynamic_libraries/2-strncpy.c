80fd8b7514605dbb17e20380d209b648bb1938c7
Hush <preciouschinazaekpere6@gmail.com>
2023-01-20 20:39:26 +0100

    Update 2-strncpy.c

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -1,24 +1,25 @@
 #include "main.h"
-
 /**
- * _strncpy - function that copies a string.
- *
- * @dest: pointer to destination char
- * @src: pointer to source char
- * @n: number of bytes
- * Return: char
- */
+*  _strncpy - C function that copies a string, including the
+*  terminating null byte, using at most an inputted number of bytes.
+*  If the length of the source string is less than the maximum byte number,
+*  the remainder of the destination string is filled with null bytes.
+*  Works identically to the standard library function `strncpy`.
+*@dest: buffer storing the string copy
+*@src:the source string
+*@n:max nummber of byte copied
+*Return: returns
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i < n && *(src + i) != '\0')
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	while (i != n)
-		dest[i++] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
