a4c43450b0558bdf4ad1b424fce5331deb23dcd0
Hush <preciouschinazaekpere6@gmail.com>
2023-01-20 20:36:42 +0100

    Update 2-strlen.c

--- 0x18-dynamic_libraries/2-strlen.c
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -1,19 +1,15 @@
 #include "main.h"
-
 /**
- * _strlen - function that returns the length of a string.
- *
- * @s: pointer to an string
- * Return: int
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
  */
-
 int _strlen(char *s)
 {
-	int i = 0;
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
 
-	while (s[i] != '\0')
-	{
-		i += 1;
-	}
-	return (i);
+return (inc);
 }
